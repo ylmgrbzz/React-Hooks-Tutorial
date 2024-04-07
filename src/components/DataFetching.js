a9dd5bb7889940579051dfd4ae418e9cb8cfeed7
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-04-07 17:47:53 +0300

    Refactor DataFetching component to fetch data based on user input

--- src/components/DataFetching.js
+++ src/components/DataFetching.js
@@ -2,11 +2,12 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const DataFetching = () => {
-  const [post, setPost] = useState([]);
+  const [post, setPost] = useState({});
+  const [id, setId] = useState(0);
 
   useEffect(() => {
     axios
-      .get("https://jsonplaceholder.typicode.com/posts")
+      .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
       .then((res) => {
         console.log(res);
         setPost(res.data);
@@ -17,9 +18,11 @@ const DataFetching = () => {
       .finally(() => {
         console.log("I am always called");
       });
-  });
+  }, [id]);
   return (
     <div>
+      <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
+      <div>{post.title}</div>
       <ul>
         {post.map((post) => (
           <li key={post.id}>{post.title}</li>
