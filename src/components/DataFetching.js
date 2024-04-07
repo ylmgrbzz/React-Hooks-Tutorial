886dbc59238c3ba88207f9e3f1e4a95fd9920b10
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-04-07 17:49:31 +0300

    Refactor DataFetching component to fetch data based on user input and handle button click event

--- src/components/DataFetching.js
+++ src/components/DataFetching.js
@@ -3,26 +3,32 @@ import axios from "axios";
 
 const DataFetching = () => {
   const [post, setPost] = useState({});
-  const [id, setId] = useState(0);
+  const [id, setId] = useState(1);
+  const [idFromButtonClick, setIdFromButtonClick] = useState(1);
 
-  useEffect(() => {
-    axios
-      .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
-      .then((res) => {
-        console.log(res);
-        setPost(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-      .finally(() => {
-        console.log("I am always called");
-      });
-  }, [id]);
+  useEffect(
+    () => {
+      axios
+        .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+        .then((res) => {
+          console.log(res);
+          setPost(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+        .finally(() => {
+          console.log("I am always called");
+        });
+    },
+    //   [id]
+    [idFromButtonClick]
+  );
   return (
     <div>
       <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
       <div>{post.title}</div>
+      <button onClick={() => setIdFromButtonClick(id)}>Fetch Post</button>
       <ul>
         {post.map((post) => (
           <li key={post.id}>{post.title}</li>
